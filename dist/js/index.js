488d2e4c534ae516307eaf7ee264ceb35a44a81d
EvgeniyLyapunov <lyapunovcs@gmail.com>
2022-09-18 17:29:33 +0300

    end work over main background, added script for humburger in mobile landscaps mode

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,18 +1,43 @@
-const hamburger = document.querySelector(".hamburger");
-const menu = document.querySelector(".menu");
-const closeElem = document.querySelector(".menu__close");
+"use strict";
 
-hamburger.addEventListener("click", () => {
-  menu.classList.add("active");
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburger = document.querySelector(".hamburger");
+  const menu = document.querySelector(".menu");
+  const closeElem = document.querySelector(".menu__close");
+
+  const menuBlock = document.querySelector(".menu__block"),
+        menuClose = document.querySelector(".menu__close"),
+        menuCloseSvg = document.querySelector(".menu__close-svg"),
+        menuLinks = document.querySelectorAll(".menu__link");
+  
+  hamburger.addEventListener("click", () => {
+    humbureg();
+  });
+  
+  closeElem.addEventListener("click", () => {
+    humbureg();
+  });
+  
+  function humbureg() {
+    const viewHeight = document.documentElement.clientHeight;
+    if(viewHeight <= 450) {
+      menuBlock.classList.toggle("active-mobile-landscape__block");
+      menuClose.classList.toggle("active-mobile-landscape__close");
+      menuCloseSvg.classList.toggle("active-mobile-landscape__close-svg");
+      menuLinks.forEach((item)=> {
+        item.classList.toggle("active-mobile-landscape__link");
+      });
+      menu.classList.toggle("active");
+    } else {
+      menu.classList.toggle("active");
+    }
+  }
+
+  const counters = document.querySelectorAll(".skills__count-point-value");
+  const countView =document.querySelectorAll(".skills__count-view");
+  
+  counters.forEach((item, i) => {
+    countView[i].style.width = item.innerHTML;
+  });
 });
 
-closeElem.addEventListener("click", () => {
-  menu.classList.remove("active");
-});
-
-const counters = document.querySelectorAll(".skills__count-point-value");
-const countView =document.querySelectorAll(".skills__count-view");
-
-counters.forEach((item, i) => {
-  countView[i].style.width = item.innerHTML;
-});
